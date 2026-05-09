f78cd738cd248cb785ad2e6f1f16d7bd52dc9543
Timileyin Akinlaja <akinlajatimileyin@gmail.com>
2026-05-09 02:47:31 +0100

    initial commit

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,10 @@ const nextConfig: NextConfig = {
         protocol: 'https',
         hostname: 'central.prag.global',
       },
+      {
+        protocol: 'https',
+        hostname: '**.prag.global',
+      },
     ],
   },
 };
