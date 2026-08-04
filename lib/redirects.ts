53681ef98786666429041c59c2f83dcf6d7d2bb4
Ralphmore <raphealobairekanmi@gmail.com>
2026-08-04 09:12:37 +0100

    Redirect

--- lib/redirects.ts
+++ lib/redirects.ts
@@ -8,4 +8,6 @@ export const LEGACY_REDIRECTS: RedirectEntry[] = [
   { source: '/battery-warranty', destination: '/warranty/battery' },
   { source: '/solar-warranty', destination: '/warranty/solar' },
   { source: '/stabilizer-warranty', destination: '/warranty/stabilizer' },
+  { source: '/product-category/:category/:subcategory', destination: '/products/:category' },
+  { source: '/product-category/:category', destination: '/products/:category' },
 ];
